570bf231176fb023f0af8075e31e6ec0efa6f090
UmitBalikci <u.balikci.bm@gmail.com>
2024-09-17 23:42:21 +0300

    add discount service

---
+++ Services/Discount/MultiShop.Discount/Dtos/CouponResultDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.Discount.Dtos
+{
+    public class CouponResultDto
+    {
+        public Guid CouponID { get; set; }
+        public string Code { get; set; }
+        public int? Rate { get; set; }
+        public bool? IsActive { get; set; }
+        public DateOnly? ValidDate { get; set; }
+    }
+}
